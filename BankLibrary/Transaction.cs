e4fe85aa79ce0c00a3708a8737e1fbdb81025332
Vamshikrishna <Vamshikrishna.Bose@Ltts.com>
2021-04-30 12:49:15 +0530

    Class library example

---
+++ BankLibrary/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get;}
+
+        public string Notes;
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
